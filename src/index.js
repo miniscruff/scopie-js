c04a2f30be064bae9f5b1270cbe24a60870f27a8
Ronnie Smith <miniscruff@hey.com>
2024-02-10 23:48:49 -0800

    slight progress made on updated scenarios

--- src/index.js
+++ src/index.js
@@ -25,15 +25,42 @@
     };
 
     const scopeSeperator = ',';
+    const arraySeperator = '|';
     const blockSeperator = '/';
     const wildcard = '*';
     const superWildcard = '**';
     const varPrefix = '@';
 
+    const invalidRegex = /[^\w\d\/@|*]/;
+
+    /**
+     * Does thing
+     * @param {object} vars
+     * @param {string} requiredScopes
+     * @param {string} actorScopes
+     */
     function isAllowed(vars, requiredScopes, actorScopes) {
-      console.log("vars", vars)
-      console.log("req", requiredScopes)
-      console.log("actor", actorScopes)
+      // console.log("vars", vars)
+      // console.log("req", requiredScopes)
+      // console.log("actor", actorScopes)
+
+      if (requiredScopes === "") {
+        throw new Error(`scopie-106 in scopes@0: scope was empty`)
+      }
+
+      if (actorScopes === "") {
+        throw new Error(`scopie-106 in actor@0: scope was empty`)
+      }
+
+      const reqInvalidIndex = requiredScopes.search(invalidRegex);
+      if (reqInvalidIndex >= 0) {
+        throw new Error(`scopie-100 in scopes@${reqInvalidIndex}: invalid character '${requiredScopes[reqInvalidIndex]}'`)
+      }
+
+      const actorInvalidIndex = actorScopes.search(invalidRegex);
+      if (actorInvalidIndex >= 0) {
+        throw new Error(`scopie-100 in actor@${actorInvalidIndex}: invalid character '${actorScopes[actorInvalidIndex]}'`)
+      }
 
       const requireds = requiredScopes.split(scopeSeperator);
       const actors = actorScopes.split(scopeSeperator);
@@ -65,12 +92,39 @@
     }
 
     function arrayEq(vars, a, b) {
-      if (a.length !== b.length && a[a.length-1] !== superWildcard) {
+      if (a.length !== b.length && a[a.length - 1] !== superWildcard) {
         return false;
       }
 
       for (let i in a) {
-        console.log(a[i], b[i]);
+        // console.log(a[i], b[i]);
+        if (a[i].indexOf(arraySeperator) >= 0) {
+          const invalidVarIndex = a[i].indexOf(varPrefix);
+          if (invalidVarIndex >= 0) {
+            let groupNameEnd = a[i].substring(invalidVarIndex + 1).search(/[/,|]/);
+            console.log("group name end", groupNameEnd)
+            console.log("extra:", a[i].substring(invalidVarIndex + 1));
+            if (groupNameEnd <= 0) {
+              groupNameEnd = undefined;
+            } else {
+              groupNameEnd += invalidVarIndex + 1
+            }
+            throw new Error(`scopie-101 in actor@${invalidVarIndex}: variable '${a[i].substring(invalidVarIndex + 1, groupNameEnd)}' found in array block`)
+          }
+
+          let matched = false;
+          for (let arrValue of a[i].split(arraySeperator)) {
+            // console.log("array comparison: ", arrValue, b[i]);
+            if (arrValue === b[i]) {
+              matched = true;
+              break;
+            }
+          }
+          if (!matched) {
+            return false;
+          }
+          continue
+        }
 
         if (a[i] === superWildcard) {
           return true;
@@ -82,7 +136,7 @@
 
         if (a[i].startsWith(varPrefix)) {
           const key = a[i].slice(1);
-          console.log("found var", key, vars[key], b[i])
+          // console.log("found var", key, vars[key], b[i])
           if (vars[key] !== b[i]) {
             return false;
           }
--- src/index.spec.js
+++ src/index.spec.js
@@ -2,27 +2,36 @@ const scenarios = require('./scopie_scenarios.json');
 const scopie = require('./index.js')
 const { isAllowed } = scopie;
 
-function testValidations() {
-  for (let tc of scenarios.validations) {
-    console.log("-- starting test:", tc.id)
+function testIsAllowed() {
+  for (let tc of scenarios.isAllowedTests) {
+    // console.log("-- starting test:", tc.id)
     try {
       const res = isAllowed(tc.variables, tc.scopes, tc.actor);
       if (tc.error !== undefined) {
-        console.error(`${tc.id} failed: expected error but got none`);
+        console.table(tc);
+        console.error(`## ${tc.id} failed: expected error but got none`);
       } else if (res !== tc.result) {
-        console.error(`${tc.id} failed: ${res} !== ${tc.result}`);
+        console.table(tc);
+        console.error(`## ${tc.id} failed: ${res} !== ${tc.result}`);
       }
     } catch (e) {
-      if (tc.error !== undefined) {
-        console.error(`${tc.id} failed: expected no error but got one`);
-      } else if (e !== tc.error) {
-        console.error(`${tc.id} failed: ${e} !== ${tc.error}`);
+      if (tc.error === undefined) {
+        console.table(tc);
+        console.error(`## ${tc.id} failed: expected no error but got one: ${e}`);
+      } else if (e.message !== tc.error) {
+        console.table(tc);
+        console.error(`## ${tc.id} failed: ${e} !== ${tc.error}`);
       }
     }
-    console.log("-- ending test:", tc.id)
+    // console.log("-- ending test:", tc.id)
+    /*
+    scopie-106 in scopes@0: scope was empty !==
+    scopie-106 in scopes@0: scope was empty
+    */
   }
 }
 
+/*
 function runBenchmarks() {
   for (let tc of scenarios.benchmarks) {
     const res = isAllowed(tc.variables, tc.scopes, tc.actor);
@@ -31,6 +40,7 @@ function runBenchmarks() {
     }
   }
 }
+*/
 
-testValidations();
-runBenchmarks();
+testIsAllowed();
+//runBenchmarks();
