04dfe4da16a8fe57c50d6a912ed37ffbc38eb76c
Ronnie Smith <miniscruff@hey.com>
2024-03-02 20:46:45 -0800

    tweak benchmark settings

--- src/index.bench.js
+++ src/index.bench.js
@@ -3,7 +3,11 @@ const scopie = require('./index.js')
 const { isAllowed } = scopie;
 
 const Bench = require('tinybench');
-const bench = new Bench.Bench({ time: 100 });
+const bench = new Bench.Bench({
+  time: 500,
+  iterations: 10000,
+  warmupTime: 1,
+});
 
 for (let tc of scenarios.benchmarks) {
   bench.add(tc.id, () => {
