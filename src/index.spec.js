e8ebb8113ac1ade268c2550de2067107fa516706
Ronnie Smith <miniscruff@hey.com>
2024-03-10 15:10:02 -0700

    properly output the test id in the each block

--- src/index.spec.js
+++ src/index.spec.js
@@ -3,25 +3,33 @@ import scenarios from './scopie_scenarios.json';
 import { isAllowed, validateScope } from '.';
 
 describe('is allowed', () => {
-  it.each(scenarios.isAllowedTests)('$tc.id', (tc) => {
-    expect.assertions(1);
-    const testFn = () => isAllowed(tc.variables, tc.scopes, tc.actor);
-    if (tc.error === undefined) {
-      expect(testFn()).toBe(tc.result);
-    } else {
-      expect(testFn).toThrow(tc.error);
-    }
-  });
+  it.each(scenarios.isAllowedTests)(
+    '$id',
+    ({
+      id, variables, scopes, actor, error, result, /* eslint-disable-line no-unused-vars */
+    }) => {
+      expect.assertions(1);
+      const testFn = () => isAllowed(variables, scopes, actor);
+      if (error === undefined) {
+        expect(testFn()).toBe(result);
+      } else {
+        expect(testFn).toThrow(error);
+      }
+    },
+  );
 });
 
 describe('is valid', () => {
-  it.each(scenarios.scopeValidTests)('$tc.id', (tc) => {
-    expect.assertions(1);
-    const err = validateScope(tc.scope);
-    if (tc.error === undefined) {
-      expect(err).toBeUndefined();
-    } else {
-      expect(err.message).toStrictEqual(tc.error);
-    }
-  });
+  it.each(scenarios.scopeValidTests)(
+    '$id',
+    ({ id, scope, error }) => { /* eslint-disable-line no-unused-vars */
+      expect.assertions(1);
+      const err = validateScope(scope);
+      if (error === undefined) {
+        expect(err).toBeUndefined();
+      } else {
+        expect(err.message).toStrictEqual(error);
+      }
+    },
+  );
 });
